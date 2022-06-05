3a7548d7b158f01d6b36982c88418e6bc458c55d
Tae-Kyoung Kim <75168305+seaworld0125@users.noreply.github.com>
2022-06-05 20:15:43 +0900

    Update Deploy.sh

--- Deploy.sh
+++ Deploy.sh
@@ -1,29 +1,40 @@
+#!/bin/sh
+
+checkLocalServerHealth() {
+  url="localhost:$1/health"
+  while :
+  do
+    status=$(curl -s $url)
+
+    if [ "$status" != "" ]; then
+      printf ">> now local:$1 server is running\n"
+      break
+    fi
+
+    sleep 1
+  done
+}
+
 # spring-main >> down
-sudo sed -i 's/localhost:8080/localhost:8080 down/g' /etc/nginx/nginx.conf
-sudo systemctl reload nginx
+sudo sed -i 's/server localhost:8080/#server localhost:8080/g' /etc/nginx/nginx.                                                                                                                                                    conf
+sudo nginx -s reload
 
 # main server stop
 sudo docker stop "spring-main"
 sudo docker rmi "seaworld0125/hoppy-be:latest"
 
 # main server rerun with latest image
-sudo docker run -d --name spring-main --rm -p 8080:8080 seaworld0125/hoppy-be:latest
+sudo docker run -d --name spring-main --rm -p 8080:8080 seaworld0125/hoppy-be:la                                                                                                                                                    test
 
-# check main server status
-until [ "$( sudo docker container inspect -f '{{.State.Running}}' spring-main )" == "true" ]
-do
-    :
-done
+# check local server health
+checkLocalServerHealth 8080
 
 # spring-main >> up
-sudo sed -i 's/localhost:8080 down/localhost:8080/g' /etc/nginx/nginx.conf
-sudo systemctl reload nginx
-
-sleep 5
+sudo sed -i 's/#server localhost:8080/server localhost:8080/g' /etc/nginx/nginx.                                                                                                                                                    conf
 
 # spring-sub >> down
-sudo sed -i 's/localhost:8081/localhost:8081 down/g' /etc/nginx/nginx.conf
-sudo systemctl reload nginx
+sudo sed -i 's/server localhost:8081/#server localhost:8081/g' /etc/nginx/nginx.                                                                                                                                                    conf
+sudo nginx -s reload
 
 # sub server stop
 sudo docker stop "spring-sub"
@@ -35,6 +46,9 @@ sudo docker image tag seaworld0125/hoppy-be:latest seaworld0125/hoppy-be:sub
 # backup server rerun
 sudo docker run -d --name spring-sub --rm -p 8081:8080 seaworld0125/hoppy-be:sub
 
+# check local server health
+checkLocalServerHealth 8081
+
 # spring-sub >> up
-sudo sed -i 's/localhost:8081 down/localhost:8081/g' /etc/nginx/nginx.conf
-sudo systemctl reload nginx
+sudo sed -i 's/#server localhost:8081/server localhost:8081/g' /etc/nginx/nginx.                                                                                                                                                    conf
+sudo nginx -s reload
